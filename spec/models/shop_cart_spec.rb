126a6f6478b154fb61417446fb3f574f6a1046e4
jay16 <jay_li@intfocus.com>
2014-02-13 12:51:23 +0800

    translate.solife

--- spec/models/shop_cart_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe ShopCart do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/fruit_zones/new.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "fruit_zones/new.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
